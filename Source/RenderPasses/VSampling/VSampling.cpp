92e1b68ec1e9a76b0ed60829d36e1927e56e6da7
kopaka <felixbruell@live.de>
2024-11-25 11:47:24 +0100

    initial vsampling commit (copy of path tracer)

---
+++ Source/RenderPasses/VSampling/VSampling.cpp
@@ -0,0 +1,189 @@
+/***************************************************************************
+ # Copyright (c) 2015-23, NVIDIA CORPORATION. All rights reserved.
+ #
+ # Redistribution and use in source and binary forms, with or without
+ # modification, are permitted provided that the following conditions
+ # are met:
+ #  * Redistributions of source code must retain the above copyright
+ #    notice, this list of conditions and the following disclaimer.
+ #  * Redistributions in binary form must reproduce the above copyright
+ #    notice, this list of conditions and the following disclaimer in the
+ #    documentation and/or other materials provided with the distribution.
+ #  * Neither the name of NVIDIA CORPORATION nor the names of its
+ #    contributors may be used to endorse or promote products derived
+ #    from this software without specific prior written permission.
+ #
+ # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS "AS IS" AND ANY
+ # EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ # PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ # CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ # EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ # PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ # PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ # OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **************************************************************************/
+#include "VSampling.h"
+
+#include "RenderGraph/RenderPassStandardFlags.h"
+
+namespace 
+{
+    const char kShaderFile[] = "RenderPasses/VSampling/VBuffer.rt.slang";
+
+    const uint32_t kMaxPayloadSizeBytes = 72u;
+    const uint32_t kMaxRecursionDepth = 1u;
+
+    const std::string kColorOut = "colorOut";
+
+    const std::string kMaxBounces = "maxBounces";
+}
+
+extern "C" FALCOR_API_EXPORT void registerPlugin(Falcor::PluginRegistry& registry)
+{
+    registry.registerClass<RenderPass, VSampling>();
+}
+
+VSampling::VSampling(ref<Device> pDevice, const Properties& props)
+    : RenderPass(pDevice)
+{
+    for(const auto& [key, value] : props)
+    {
+        if(key == kMaxBounces) mMaxBounces = value;
+        else logWarning("Unknown field '" + key + "' in VSampling pass");
+    }
+
+    mpSampleGenerator = SampleGenerator::create(mpDevice, SAMPLE_GENERATOR_UNIFORM);
+}
+
+Properties VSampling::getProperties() const
+{
+    Properties props;
+    props[kMaxBounces] = mMaxBounces;
+    return props;
+}
+
+RenderPassReflection VSampling::reflect(const CompileData& compileData)
+{
+    // Define the required resources here
+    RenderPassReflection reflector;
+    reflector.addOutput(kColorOut, "color");
+    return reflector;
+}
+
+void VSampling::execute(RenderContext* pRenderContext, const RenderData& renderData)
+{
+    auto& dict = renderData.getDictionary();
+    if(mOptionsChanged)
+    {
+        auto flags = dict.getValue(kRenderPassRefreshFlags, RenderPassRefreshFlags::None);
+        dict[Falcor::kRenderPassRefreshFlags] = flags | Falcor::RenderPassRefreshFlags::RenderOptionsChanged;
+        mOptionsChanged = false;
+    }
+
+    auto pColorOut = renderData[kColorOut]->asTexture();
+
+    if(!mpScene)
+    {
+        pRenderContext->clearTexture(pColorOut.get(), float4(0.0f, 0.0f, 0.0f, 1.0f));
+        return;
+    }
+
+    // Request the light collection if emissive lights are enabled.
+    if (mpScene->getRenderSettings().useEmissiveLights)
+    {
+        mpScene->getLightCollection(pRenderContext);
+    }
+
+    mTracer.pProgram->addDefine("MAX_BOUNCES", std::to_string(mMaxBounces));
+    mTracer.pProgram->addDefine("USE_ANALYTIC_LIGHTS", mpScene->useAnalyticLights() ? "1" : "0");
+    mTracer.pProgram->addDefine("USE_EMISSIVE_LIGHTS", mpScene->useEmissiveLights() ? "1" : "0");
+    mTracer.pProgram->addDefine("USE_ENV_LIGHT", mpScene->useEnvLight() ? "1" : "0");
+    mTracer.pProgram->addDefine("USE_ENV_BACKGROUND", mpScene->useEnvBackground() ? "1" : "0");
+    mTracer.pProgram->addDefine("USE_RUSSIAN_ROULETTE_PATH", mUseRussianRoulettePath ? "1" : "0");
+
+    if (!mTracer.pVars)
+    {
+        // Configure program.
+        mTracer.pProgram->addDefines(mpSampleGenerator->getDefines());
+        mTracer.pProgram->setTypeConformances(mpScene->getTypeConformances());
+
+        // Create program variables for the current program.
+        // This may trigger shader compilation. If it fails, throw an exception to abort rendering.
+        mTracer.pVars = RtProgramVars::create(mpDevice, mTracer.pProgram, mTracer.pBindingTable);
+
+        // Bind utility classes into shared data.
+        auto var = mTracer.pVars->getRootVar();
+        mpSampleGenerator->setShaderData(var);
+    }
+
+    auto var = mTracer.pVars->getRootVar();
+    var["CB"]["gFrameCount"] = mFrameCount;
+    //var["CB"]["gPRNGDimension"] = dict.keyExists(kRenderPassPRNGDimension) ? dict[kRenderPassPRNGDimension] : 0u;
+    var["gColorOut"] = pColorOut;
+
+    const uint2 targetDim = renderData.getDefaultTextureDims();
+    mpScene->raytrace(pRenderContext, mTracer.pProgram.get(), mTracer.pVars, uint3(targetDim, 1));
+
+    mFrameCount++;
+}
+
+void VSampling::renderUI(Gui::Widgets& widget)
+{
+    bool dirty = false;
+
+    dirty |= widget.var("Max bounces", mMaxBounces, 0u, 1u << 16);
+    widget.tooltip("Maximum path length for indirect illumination.\n0 = direct only\n1 = one indirect bounce etc.", true);
+
+    dirty |= widget.checkbox("Russian roulette path", mUseRussianRoulettePath);
+    widget.tooltip("Use Russian roulette for path termination", true);
+
+    if (dirty)
+    {
+        mOptionsChanged = true;
+    }
+}
+
+void VSampling::setScene(RenderContext* pRenderContext, const ref<Scene>& pScene)
+{
+    // Clear data for previous scene.
+    // After changing scene, the raytracing program should to be recreated.
+    mTracer.pProgram = nullptr;
+    mTracer.pBindingTable = nullptr;
+    mTracer.pVars = nullptr;
+    mFrameCount = 0;
+
+    // Set new scene.
+    mpScene = pScene;
+
+    if (mpScene)
+    {
+        if (pScene->hasGeometryType(Scene::GeometryType::Custom))
+        {
+            logWarning("MinimalPathTracer: This render pass does not support custom primitives.");
+        }
+
+        // Create ray tracing program.
+        RtProgram::Desc desc;
+        desc.addShaderModules(mpScene->getShaderModules());
+        desc.addShaderLibrary(kShaderFile);
+        desc.setMaxPayloadSize(kMaxPayloadSizeBytes);
+        desc.setMaxAttributeSize(mpScene->getRaytracingMaxAttributeSize());
+        desc.setMaxTraceRecursionDepth(kMaxRecursionDepth);
+
+        mTracer.pBindingTable = RtBindingTable::create(2, 2, mpScene->getGeometryCount());
+        auto& sbt = mTracer.pBindingTable;
+        sbt->setRayGen(desc.addRayGen("rayGen"));
+        sbt->setMiss(0, desc.addMiss("miss"));
+
+        if (mpScene->hasGeometryType(Scene::GeometryType::TriangleMesh))
+        {
+            sbt->setHitGroup(0, mpScene->getGeometryIDs(Scene::GeometryType::TriangleMesh), desc.addHitGroup("closestHit", "anyHit"));
+        }
+
+        mTracer.pProgram = RtProgram::create(mpDevice, desc, mpScene->getSceneDefines());
+    }
+
+}
