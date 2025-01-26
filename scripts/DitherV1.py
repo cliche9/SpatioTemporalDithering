24daf9904b883b5103da7051cb51e9c3793ad9c5
kopaka <felixbruelllive.de>
2025-01-26 16:47:06 +0100

    fixed color output for dlss (force non srgb buffer)

--- scripts/DitherV1.py
+++ scripts/DitherV1.py
@@ -2,7 +2,7 @@ from pathlib import WindowsPath, PosixPath
 from falcor import *
 
 def render_graph_DefaultRenderGraph():
-    g = RenderGraph('DefaultRenderGraph')
+    g = RenderGraph('Dither')
     g.create_pass('VBufferLighting', 'VBufferLighting', {'envMapIntensity': 0.25, 'ambientIntensity': 0.25, 'lightIntensity': 0.5, 'envMapMirror': False})
     g.create_pass('DitherVBuffer', 'DitherVBuffer', {})
     g.create_pass('ToneMapper', 'ToneMapper', {'outputSize': 'Default', 'useSceneMetadata': True, 'exposureCompensation': 0.0, 'autoExposure': False, 'filmSpeed': 100.0, 'whiteBalance': False, 'whitePoint': 6500.0, 'operator': 'Aces', 'clamp': True, 'whiteMaxLuminance': 1.0, 'whiteScale': 11.199999809265137, 'fNumber': 1.0, 'shutter': 1.0, 'exposureMode': 'AperturePriority'})
@@ -10,17 +10,19 @@ def render_graph_DefaultRenderGraph():
     g.create_pass('DLSSPass', 'DLSSPass', {'enabled': True, 'outputSize': 'Default', 'profile': 'DLAA', 'motionVectorScale': 'Relative', 'isHDR': True, 'useJitteredMV': False, 'sharpness': 0.0, 'exposure': 0.0})
     g.create_pass('UnpackVBuffer', 'UnpackVBuffer', {})
     g.create_pass('RayShadow', 'RayShadow', {})
+    g.create_pass('ImageEquation', 'ImageEquation', {'formula': 'I0[xy]', 'format': 'RGBA32Float'})
     g.add_edge('DitherVBuffer.vbuffer', 'VBufferLighting.vbuffer')
     g.add_edge('VBufferLighting.color', 'ToneMapper.src')
-    g.add_edge('ToneMapper.dst', 'TAA.colorIn')
     g.add_edge('DitherVBuffer.mvec', 'TAA.motionVecs')
-    g.add_edge('ToneMapper.dst', 'DLSSPass.color')
     g.add_edge('DitherVBuffer.mvec', 'DLSSPass.mvec')
     g.add_edge('DitherVBuffer.depth', 'DLSSPass.depth')
     g.add_edge('DitherVBuffer.vbuffer', 'UnpackVBuffer.vbuffer')
     g.add_edge('UnpackVBuffer.posW', 'RayShadow.posW')
     g.add_edge('UnpackVBuffer.normalW', 'RayShadow.normalW')
     g.add_edge('RayShadow.visibility', 'VBufferLighting.visibilityBuffer')
+    g.add_edge('ToneMapper.dst', 'ImageEquation.I0')
+    g.add_edge('ImageEquation.out', 'DLSSPass.color')
+    g.add_edge('ImageEquation.out', 'TAA.colorIn')
     g.mark_output('DLSSPass.output')
     g.mark_output('TAA.taaOut')
     return g
